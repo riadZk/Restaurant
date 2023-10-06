d293355da22a8317996c60fc5831c24f1b3dd414
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-10-06 15:13:54 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,7 @@ const fonts = East_Sea_Dokdo({ subsets: ['latin'], weight: "400" })
 const font = Shalimar({ subsets: ['latin'], weight: "400" })
 
 export default function Home() {
-
-  const src = '/images/pexels-lucas-allmann-1378424.jpg';
+  const src = '/images/lucas.jpg';
 
   return (
     <div className='h-auto'>
@@ -19,9 +18,13 @@ export default function Home() {
         <div className='h-screen w-full' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
           <div className='flex h-[75%] w-full justify-end pb-20 font-bold text-white flex-col items-center '>
             <h1 className='text-7xl text-white'>
-              <span className={font.className}>SavorSphere</span>
+              <span className={font.className}>
+                    SavorSphere
+              </span>
             </h1>
-            <p className='text-white text-3xl'><Word /></p>
+            <p className='text-white text-3xl'>
+                <Word />
+            </p>
           </div>
           <div className='w-full h-[25%]'>
             <Slider />
